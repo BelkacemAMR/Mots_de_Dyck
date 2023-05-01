00fb1fafd6446df4009a96ce9ad019da9bf6558d
Belkacem <abel-and-co@hotmail.com>
2023-05-01 23:08:02 +0200

    version 2

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-def is_a_dyck_word(word: str) -> bool:
+# def is_a_dyck_word(word: str) -> bool:
+#     stack = []
+#     for c in word:
+#         if c == '(':
+#             stack.append(c)
+#         elif c == ')':
+#             if len(stack) == 0:
+#                 return False
+#             stack.pop()
+#         else:
+#             return False
+#     return len(stack) == 0
+
+def is_a_dyck_word(word):
+    opening_symbols = set('([{‹«')
+    closing_symbols = set(')]}›»')
     stack = []
     for c in word:
-        if c == '(':
+        if c in opening_symbols:
             stack.append(c)
-        elif c == ')':
-            if len(stack) == 0:
+        elif c in closing_symbols:
+            if not stack:
                 return False
-            stack.pop()
-        else:
-            return False
-    return len(stack) == 0
+            elif opening_symbols.index(stack.pop()) != closing_symbols.index(c):
+                return False
+    return not stack
+
