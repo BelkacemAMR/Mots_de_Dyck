99b82f6fc52c5d34ae10d611cf517ca72c354f10
Belkacem <abel-and-co@hotmail.com>
2023-05-01 22:47:04 +0200

    version initiale

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# Test
-import pandas
-import numpy
+def is_a_dyck_word(word: str) -> bool:
+    stack = []
+    for c in word:
+        if c == '(':
+            stack.append(c)
+        elif c == ')':
+            if len(stack) == 0:
+                return False
+            stack.pop()
+        else:
+            return False
+    return len(stack) == 0
