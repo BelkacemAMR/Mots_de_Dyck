f66f904a6e1d1ba5f9c5883ee196641200582604
Belkacem <abel-and-co@hotmail.com>
2023-05-02 19:51:40 +0200

    version 3

--- main.py
+++ main.py
@@ -15,23 +15,37 @@
 #     return len(stack) == 0
 
 
-## Version 2 Provisoire :
+# ## Version 2 Provisoire :
+#
+# def is_a_dyck_word(word):
+#     opening_symbols = set('([{‹«')
+#     closing_symbols = set(')]}›»')
+#     stack = []
+#     for c in word:
+#         if c in opening_symbols:
+#             stack.append(c)
+#         elif c in closing_symbols:
+#             if not stack:
+#                 return False
+#             elif opening_symbols.index(stack.pop()) != closing_symbols.index(c):
+#                 return False
+#     return not stack
+
+
+# Version 3 :
 
 def is_a_dyck_word(word):
-    opening_symbols = set('([{‹«')
-    closing_symbols = set(')]}›»')
+    opening_symbols = {'[', '{', '(', '‹', '«', '“', '‘'}
+    closing_symbols = {']', '}', ')', '›', '»', '”', '’'}
     stack = []
     for c in word:
         if c in opening_symbols:
             stack.append(c)
         elif c in closing_symbols:
-            if not stack:
-                return False
-            elif opening_symbols.index(stack.pop()) != closing_symbols.index(c):
+            if not stack or opening_symbols.index(stack.pop()) != closing_symbols.index(c):
                 return False
     return not stack
 
-
 # Exemple d'utilisation :
 
-
+is_a_dyck_word('f(((())))')
